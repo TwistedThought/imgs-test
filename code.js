494d75beefe067b2ad7b8526fa771f3a64640d7c
TwistedThought <alexxxgf@mail.ru>
2017-02-11 18:04:29 +0300

    done

---
+++ code.js
@@ -0,0 +1,96 @@
+function move(e) {
+
+  var elem = document.getElementById(e.target.id);
+  var zindex = document.getElementById('z');
+
+  elem.onmousemove = function(e) {
+    var coords = getCoords(elem);
+    var shiftX = e.pageX - coords.left;
+    var shiftY = e.pageY - coords.top;
+
+    if (shiftX > (e.target.clientWidth-30) && shiftY > (e.target.clientHeight-30)) {
+      elem.style.cursor = 'nw-resize';
+
+      elem.onmousedown = function(e) {
+
+        document.onmousemove = function(e) {
+          elem.style.width = e.pageX - coords.left + 'px';
+        };
+
+        document.onmouseup = function(e) {
+          document.onmousemove = null;
+          document.onmouseup = null;
+        }
+      };
+    }
+    else if (shiftY < 20) {
+      elem.style.cursor = 'row-resize';
+      elem.onmousedown = function(e) {
+        var start = e.pageY;
+        document.onmousemove = function(e) {
+          document.body.style.cursor = 'row-resize';
+          elem.style.cursor = 'row-resize';
+          elem.style.zIndex = start - e.pageY;
+          zindex.innerHTML = 'z-index: ' + (start - e.pageY);
+          zindex.style.top = e.pageY-15 + 'px';
+          zindex.style.left = e.pageX+10 + 'px';
+          zindex.style.display = 'inline';
+        }
+        document.onmouseup = function(e) {
+          document.body.style.cursor = 'default';
+          document.onmousemove = null;
+          document.onmouseup = null;
+          zindex.style.display = 'none';
+        }
+      }
+    }
+    else {
+      elem.style.cursor = 'move';
+
+      elem.onmousedown = function(e) {
+
+        var coords = getCoords(elem);
+        var shiftX = e.pageX - coords.left;
+        var shiftY = e.pageY - coords.top;
+
+        elem.style.position = 'absolute';
+        document.body.appendChild(elem);
+        moveAt(e);
+
+        elem.style.zIndex = 10;
+
+        function moveAt(e) {
+          elem.style.left = e.pageX - shiftX + 'px';
+          elem.style.top = e.pageY - shiftY + 'px';
+        }
+
+        document.onmousemove = function(e) {
+          moveAt(e);
+        };
+
+        elem.onmouseup = function() {
+          document.onmousemove = null;
+          elem.onmouseup = null;
+        };
+
+      }
+
+      elem.ondragstart = function() {
+        return false;
+      };
+    };
+  }
+
+}
+
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset,
+    right: box.right + pageXOffset,
+    bottom: box.botttom + pageYOffset
+  };
+
+}
